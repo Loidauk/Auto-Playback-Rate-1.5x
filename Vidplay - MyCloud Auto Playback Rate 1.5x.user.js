91e39bc7e827547d55f77622333ae6a797b9c276
Loidauk <76977614+Loidauk@users.noreply.github.com>
2024-06-08 18:44:06 +0200

    Update Vidplay - MyCloud Auto Playback Rate 1.5x.user.js

--- Vidplay - MyCloud Auto Playback Rate 1.5x.user.js
+++ Vidplay - MyCloud Auto Playback Rate 1.5x.user.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name         Vidplay / MyCloud Auto Playback Rate 1.5x
+// @namespace    https://github.com
 // @version      0.1
 // @description  Automatically set playback rate to 1.5x on Vidplay and MyCloud videos
 // @author       TellerTellerMoose
